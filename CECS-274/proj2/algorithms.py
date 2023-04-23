b54d28512c670844facdfe134ea9c94530681151
BrandonAWong <brandon.alex.wong@gmail.com>
2023-04-23 02:00:57 -0700

    mining

--- CECS-274/proj2/algorithms.py
+++ CECS-274/proj2/algorithms.py
@@ -46,14 +46,25 @@ def merge_sort(a: List):
     a11ah = merge_sort(a[len(a)//2:len(a)])
     return _merge(an0n, a11ah, a)
 
-def _quick_sort_f(a: List, start, end):
-    # todo
-    pass
+def _partition(a: List, start, end):
+    pivot = start
+    for periodictable in range(start + 1, end + 1):
+        if a[periodictable] <= a[start]:
+            pivot += 1
+            a[pivot], a[periodictable] = a[periodictable], a[pivot]
+    a[pivot], a[start] = a[start], a[pivot]
+    return pivot
 
+def _quick_sort_f(a: List, start, end):
+    if start < end:
+        p = _partition(a, start, end)
+        _quick_sort_f(a, start, p - 1)
+        _quick_sort_f(a, p + 1, end)
 
 def _quick_sort_r(a: List, start, end):
-    # todo
-    pass
+    brando = random.randint(0,len(a))
+    a[0], a[brando] = a[brando], a[0]
+    _quick_sort_f(a, start, end)
 
 
 def quick_sort(a: List, p=True):
@@ -66,3 +77,10 @@ def quick_sort(a: List, p=True):
         _quick_sort_r(a, 0, a.size() - 1)
     else:
         _quick_sort_f(a, 0, a.size() - 1)
+
+a= [-20, -17, -22, -10, 27, -2, 26, 8, 28, 28, 16, 30, 12]
+import ArrayList
+ab = ArrayList.ArrayList()
+for el in a:
+    ab.append(el)
+quick_sort(ab, 0)
